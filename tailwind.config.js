e894ce63a9f129c5968043889352962dc86615cc
Firgenius Kolose Hombore <firgeniusigeni7799@gmail.com>
2024-09-01 23:13:13 +0900

    Sudah Selesai Pertemuan 8

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-darkMode:'class',
-  content: ["./public/**/*.{html,js}","./src/**/*.{html,js}"],
-theme: {
+  darkMode: 'class',
+  content: ["./public/**/*.{html,js}", "./src/**/*.{html,js}"],
+  theme: {
     extend: {
-  spacing:{"15":"4rem"},
-  fontFamily:{'inter':"Inter"},
-    colors: {
-       "primary":"#ffff00",
-      "secondary":"red"
-      },animation:{
-'spin-slow':'spin 5s linear infinite'
-,'goyang':'goyang 1s ease-in-out infinite',},
-
-keyframes:{goyang:{
-'0%,100%':{transform:'rotate (-3deg)'},
-'50%':{transform:'rotate (3deg)'}
-}},
-
-outlineWidth: {3: '3px',},
-ringWidth:{3:"3px",},
-margin:{30:"7.5rem",},
-},
+      spacing: {
+        "15": "4rem",
+      },
+      fontFamily: {
+        'inter': "Inter",
+      },
+      colors: {
+        "primary": "#ffff00",
+        "secondary": "red",
+      },
+      animation: {
+        'spin-slow': 'spin 5s linear infinite',
+        'goyang': 'goyang 2500ms ease-in-out infinite',
+      },
+      keyframes: {
+        'goyang': {
+          '0%,100%': { transform:'rotate(-4deg)' },
+          '50%': { transform:'rotate(4deg)' },
+        },
+      },
+      outlineWidth: {
+        '3': '3px',
+      },
+      ringWidth: {
+        '3': '3px',
+      },
+      margin: {
+        '30': '7.5rem',
+      },
+    },
   },
   plugins: [],
 };
-
